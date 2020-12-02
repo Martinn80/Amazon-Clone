27ef10ba85cbce5f15961a95ff5500c071784804
Martin Vuong Nguyen <ngumartin@gmail.com>
2020-12-02 11:56:41 -0800

    Organizing files and removing logos.

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDHEAFda4AV07nH6U6MkLt3KtL59pIgkoI",
+    authDomain: "clone-c5dfb.firebaseapp.com",
+    databaseURL: "https://clone-c5dfb.firebaseio.com",
+    projectId: "clone-c5dfb",
+    storageBucket: "clone-c5dfb.appspot.com",
+    messagingSenderId: "709249367332",
+    appId: "1:709249367332:web:72d112643ad01423bd07b1",
+    measurementId: "G-2PYT4CDQJZ"
+};
